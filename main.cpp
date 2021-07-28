801f292a211fc2f40565396df5441f45fab06d03
Kelevra <kelevra.selevin@mail.ru>
2021-07-29 00:20:52 +0700

    Задал цель. Необходимо сделать метод, который будет переводить текст в массив

--- main.cpp
+++ main.cpp
@@ -2,17 +2,24 @@
 
 using namespace std;
 
-int main()
-{
-    int matrixSymbol[3][15] = {
+ int matrixSymbol[3][15] = {
         {1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1},
         {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1},
         {1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1}};
 
 
+
+void ShowText(int *matrixOutput, string str)
+{
+
+}
+
+int main()
+{
+   
     int matrixOutput[85];
 
-    
+    ShowText(matrixOutput, "123");
 
 
     return 0;
