e73465bac58a1f00bce131e36ea9a09d39f9e27d
Kelevra <kelevra.selevin@mail.ru>
2021-08-07 13:42:34 +0700

    Вроде поправил настройки компилятора.

--- main.cpp
+++ main.cpp
@@ -1,81 +1,25 @@
 #include <iostream>
-#include <string.h>
 
-using namespace std;
-
-char parseString[100];
-
-int matrixSymbol[3][15] = {
-    {1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1},
-    {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1},
-    {1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1}};
-
-/* class LedText
+class LedText
 {
 public:
-    LedText();
-
     // Длина строки
     int8_t lenghtString = 0;
 
-    // Временна строка для работы
-    char tempString[100] = "";
-    
-    void SetString(char *inputString, int lenght)
+    void GetString()
     {
-        for(int i = 0; i < lenght; i++)
-        {
-            tempString[i] = inputString[i];
-        }
-
-        ToLowerString();
+        std::cout << "This message from Class" << std::endl;
     }
 
-    void GetString()
-    { std::cout << "wetetet" << std::endl;}
- 
-    //private:
-    // Принимаю строку.
-    // Привожу ее к нижнему регистру
-         void ToLowerString()
-    {
-        for (int i = 0; i < strlen(tempString); i++)
-        {
-            tempString[i] = tolower(tempString[i]);
-        }
-    } 
+private:
+    int a;
 };
- */
+
 int main()
 {
-std::cout << "Huli" << std::endl;
-//    LedText text;
+    LedText text;
 
-  //  text.GetString();
+    text.GetString();
 
-    //char test[100] = "sDf g#43 hJ";
-
-    //text.SetString(test, strlen(test));
-
-    //text.GetString();
-    //char HH[4] = "237";
-
-    //int yyy = atoi(HH[1]) ;
-
-    //std::cout << yyy + 67 << std::endl;
-
-    //  char stringTest[] = itoa(12);
-
-    // strcat(stringTest, " ");
-
-    //    ParseString("11:26 RewRITE test");
-
-    /*  for (int i = 0; i < strlen(parseString); i++)
-    {
-        std::cout << parseString[i] << "><";
-    }
-
-    std::cout << "" << std::endl;
- */
     return 0;
 }
