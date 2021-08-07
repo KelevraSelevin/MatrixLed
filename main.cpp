d22adf4bf9adda0babe9c6f6540cdd151251eec7
Kelevra <kelevra.selevin@mail.ru>
2021-08-07 14:11:17 +0700

    Начинаю наконц писать код )

--- main.cpp
+++ main.cpp
@@ -1,14 +1,46 @@
 #include <iostream>
 
+#include <cstring>
+
+char parseString[100];
+
+int matrixSymbol[3][15] = {
+    {1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1},
+    {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1},
+    {1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1}};
+
+
 class LedText
 {
 public:
+
     // Длина строки
     int8_t lenghtString = 0;
 
+    // Временна строка для работы
+    char tempString[100] = "";
+
+    void SetString(char *inputString, int lenght)
+    {
+        for (int i = 0; i < lenght; i++)
+        {
+            tempString[i] = inputString[i];
+        }
+
+        ToLowerString();
+    }
+
+    void ToLowerString()
+    {
+        for (int i = 0; i < strlen(tempString); i++)
+        {
+            tempString[i] = tolower(tempString[i]);
+        }
+    }
+
     void GetString()
     {
-        std::cout << "This message from Class" << std::endl;
+        std::cout << tempString << std::endl;
     }
 
 private:
@@ -18,6 +50,10 @@ private:
 int main()
 {
     LedText text;
+
+    char stringInput[100] = "HellO WoRlD";
+
+    text.SetString(stringInput, strlen(stringInput));
 
     text.GetString();
 
